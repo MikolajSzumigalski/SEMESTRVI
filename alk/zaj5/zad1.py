a53b6d7f0ffc607a2365b13017e778a765531d6f
MikolajSzumigalski <m.szumigalski@op.pl>
2018-03-30 17:24:05 +0200

    alk

--- alk/zaj5/zad1.py
+++ alk/zaj5/zad1.py
@@ -1,22 +1,28 @@
 n = int(input("Podaj liczbe n (maksymalna wartosc elementu w podzbiorze): "))
 k = int(input("Podaj k (ilość elementow w podzbiorze): "))
 
-def SuperPrint(n, a):
-    for i in range(0, n):
+def SuperPrint(n,k, a):
+    for i in range(0, n+1):
         print(A[i])
 
 A = []
 P = []
 POM = []
-for i in range (0, n):
-    A.append([ [[]] for j in range(0,i+2) ])
+for i in range (0, n+1):
+    A.append([ [[]] for j in range(0,i+1) ])
 
-for i in range (0, n):
-    P.append(i+1)
+for i in range (1, n+1):
+    P.append(i)
     A[i][len(A[i])-1] = [list(P)]
 
-for i in range(1, n):
-    for j in range(1, i+1):
+for i in range(2, n+1):
+    for j in range(1, i):
         POM = list(reversed(A[i-1][j-1]))
-        A[i][j] = list(A[i-1][j])+ list([x + [i+1] for x in POM])
-SuperPrint(n, A)
+        A[i][j] = list(A[i-1][j])+ list([x + [i] for x in POM])
+
+#SuperPrint(n,k, A)
+# wiem, że funkcja SuperPrint jest niepotrzebna
+# ale całkiem ładnie wyświetla całą  tablicę więc ją zostawiłem
+# mam nadzieję, że to nie problem
+
+print(A[n][k])
